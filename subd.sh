b373b11a1b9b52e12137ebb2aefb9786b7da0c35
josh green <bcsjoshgreen25@gmail.com>
2023-08-12 01:04:58 +0300

    Update subd.sh

--- subd.sh
+++ subd.sh
@@ -24,18 +24,19 @@ echo -en "\e[1;33m\t\t\t[+] Harvesting subdomains\e[0m\n\n"
 if [ ! -d $1 ]; then
 mkdir $1
 fi
-
-if [ ! -d $1/recon/ ]; then
-echo -en "creating a \e[1;34m $1 / recon\e[0m directory on your location\n\n"
-mkdir -p $1/recon
-fi
+recon(){
+	if [ ! -d $1/recon/ ]; then
+	echo -en "creating a \e[1;34m $1 \e[0m directory on your program location location\n\n"
+	mkdir $1
+	fi
+}
 
 subdomain=$1
-assetfinder $subdomain | httprobe >> $1/recon/sub.txt
+assetfinder $subdomain | httprobe >> $1/subdomains.txt
 
-if [ -e $1/recon/sub.txt ]; then
+if [ -e $1/subdomains.txt ]; then
 echo -e "\t\t\e[1;34mHarvesting hosts for the domains generated\e[0m\n\n"
-for host in $(cat $1/recon/sub.txt); do host $host; done | awk -F "has address" '{print $1 $2}' >> $1/recon/sub+ip.txt
+for host in $(cat $1/subdomains.txt); do host $host; done | awk -F "has address" '{print $1 $2}' >> $1/subdomains-hosts.txt
 
 fi
 echo -en "\t\e[1;32m[+]Done[+]\e[0m check \e[1;32m$1 \e[0m/ recon for your \e[1;32mdata\e[0m"
